2ec51c2e55a1225d4b7d2192adb2b4b7e9cdb6d2
MuhammadAbuBakarMemon <muhammadabubakermemon@gmail.com>
2025-09-30 12:04:21 +0500

    Add files via upload

--- LabTasks/Week00/Q4.cpp
+++ LabTasks/Week00/Q4.cpp
@@ -3,10 +3,39 @@
 // temperatures, then display them along with the average temperature. Finally, free the memory.
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main(void)
 {
-    
+
+    double *ray = new double[7];
+
+    cout << "Please start ntering the daily temperatures: " << endl;
+
+    for (int m = 0; m < 7; m++)
+    {
+        cout << "Day Number#0" << m + 1 <<  ": " << endl;
+        cin >> *(ray+m);
+    }
+
+    cout << "Temperatures for all days successfully feeded...." << endl;
+
+    double avg = 0.00;
+
+    for (int m = 0; m < 7; m++)
+    {
+        avg += ray[m];
+    }
+
+    cout << "The average temperture is: " << fixed << setprecision(2) << (avg/7) << endl;
+
+    for (int m = 0; m < 7; m++)
+    {
+        cout << "Temperature for day - 0" << m << ": " << ray[m] << endl;
+    }
+
+    delete[] ray;
+
     return 0;
 }
