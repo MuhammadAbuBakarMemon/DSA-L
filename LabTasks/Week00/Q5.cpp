886d62e77622d65d57a0c5c410aa62e63d4a6a76
MuhammadAbuBakarMemon <muhammadabubakermemon@gmail.com>
2025-09-30 13:14:58 +0500

    Add files via upload

--- LabTasks/Week00/Q5.cpp
+++ LabTasks/Week00/Q5.cpp
@@ -6,10 +6,98 @@
 // difference.
 
 #include <iostream>
+#include <string>
+#include <cstring>
 using namespace std;
+
+class Teacher
+{
+
+    public:
+        char* teachersName;
+        char* teachersSubject;
+        int yearsOfExperience; 
+
+        Teacher(const string& m, const string& n, int o) : yearsOfExperience(o)
+        {
+            // boys we discovered a new technique today////
+            teachersName = new char[m.size()];
+            teachersSubject = new char[strlen(n) + 1];
+        }
+
+        Teacher(const Teacher& obj)
+        {
+            this->teachersName = new char[strlen(obj.teachersName) + 1];
+
+            for (int m = 0; m < strlen(obj.teachersName); m++)
+            {
+                teachersName[m] = obj.teachersName[m];
+            }
+
+            this->tecahersSubject = new char[strlen(obj.teachersSubject) + 1]
+
+            for (int m = 0; m < strlen(obj.teachersSubject); m++)
+            {
+                teachersName[m] = obj.teachersName[m];
+            }
+
+            this->yearsOfExperience = obj.yearsOfExperience;
+
+            cout << "Copy constructor was called////" << endl;
+            cout << "Object Successfully copied////" << endl;
+        }
+
+        Teacher& operator= (const Teacher& obj)
+        {
+            if (*this == obj)
+            {
+                cout << "Can not call copy assignment opertor on same object////" << endl;
+                return this;
+            }
+
+            this->teachersName = new char[strlen(obj.teachersName) + 1];
+
+            for (int m = 0; m < strlen(obj.teachersName); m++)
+            {
+                this->teachersName[m] = obj.teachersName[m];
+            }
+
+            this->teachersSubject = new char[strlen(obj.tecahersSubject) + 1];
+
+            for(int m = 0; m < strlen(obj.teachersSubject); m++)
+            {
+                this->teachersSubject[m] = obj.teachersSubject[m];
+            }
+
+            this->yearsOfExperience = obj.yearsOfExperience;
+
+            cout << "Copy assignment opertor successfully called////\n";
+            return this;
+
+        }
+
+        ~Teacher()
+        {
+            delete[] teachersName;
+            delete[] teachersSubject;
+        }
+
+        void display(void)
+        {
+            cout << "Teacher's Name: " << this->teachersName << endl;
+            cout << "Teacher's Subject: " << this->teachersSubject << endl;
+            cout << "Teacher's Years Of Experience: " << this->yearsOfExperience << endl;
+        }
+};
 
 int main(void)
 {
-    
+
+    Teacher t1("Abu Bakar", "Programming FUndamentals", 23);
+    Teacher t2("Waqar", "Maths", 3);
+
+    t1.display();
+    t2.display();
+
     return 0;
 }
