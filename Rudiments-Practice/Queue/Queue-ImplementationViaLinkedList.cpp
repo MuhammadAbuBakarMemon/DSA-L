fb9a85546b2913728c9f85972cb276b69f9c08de
Muhammad Abu Bakar <muhammadabubakermemon@gmail.com>
2025-11-20 17:49:39 +0500

    It's that time of the year my friends.....

--- Rudiments-Practice/Queue/Queue-ImplementationViaLinkedList.cpp
+++ Rudiments-Practice/Queue/Queue-ImplementationViaLinkedList.cpp
@@ -87,6 +87,26 @@ class Queue
                 Node* temp = front;
                 front = front->next;
 
+                // This is an edge case 
+
+                /*
+                
+                See this is c++ not java or pyhton 
+
+                once we dynamically allocate a memory via new we need to ensure we delete that memory, because otherwise we have 
+                a critical memory leak in our program, when all the queue was filled and we one by one dequeued an element we reached the rear of the 
+                queue 
+                once this happens the front and rear both point to the last Node in the queue 
+                now performing a dequeue function means that the 
+                front pointer = NULL
+                rear still points to the node we just deleted 
+                rear becomes a dangling pointer, as we deleted temp pointer which also pointed to the same node 
+                so when we deleted temp, this pointer and the node it pointed too both got deleted, but rear still points to a the invalid 
+                memory address and now  becomes a dangling pointer   
+                
+                */
+
+                // Ensure if queue is now empty, then rear is also null
                 if (front == NULL)
                 {
                     rear = nullptr;
@@ -94,6 +114,14 @@ class Queue
 
                 delete temp;
             }
+
+            /*
+            But didn't the rear pointer already become null when I deleted the temporary pointer?
+            
+            nope delete temp destroys the house (the memory) but it does not erase the addres written in your address book for the rear pointer 
+            so rear is a dnagling pointer as it is pointing to a location in the memory that no longer belongs to our program 
+
+            */
 
         }
 
