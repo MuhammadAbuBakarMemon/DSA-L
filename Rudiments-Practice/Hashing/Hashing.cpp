48b4536b88634e5a9426edebccd69da3e3f0581d
Muhammad Abu Bakar <muhammadabubakermemon@gmail.com>
2025-11-28 02:50:48 +0500

    I am a failure after what happened in hackathon, plus my favoraite teacher won't be here next semester....

--- Rudiments-Practice/Hashing/Hashing.cpp
+++ Rudiments-Practice/Hashing/Hashing.cpp
@@ -115,6 +115,41 @@ class HashTable
             delete[] table;
         }
 
+        // void insert(string key, int value)
+        // {
+        //     if (getLoadFactor() > 0.7)
+        //     {
+        //         rehash();
+        //     }
+
+        //     int index = hashFucntion(key);
+
+        //     HashNode* trav = table[index];
+        //     // HashNode* prev = NULL;  //this is useless since we perform insertion at the front/head of the list 
+
+        //     while (trav != NULL)
+        //     {
+        //         if (trav->key == key)
+        //         {
+        //             trav->value = value;
+        //             cout << "key [" << key << "] updated." << endl;
+        //             return;
+        //         }
+
+        //         //prev = trav;  //this is useless since we perform insertion at the front/head of the list 
+        //         trav = trav->next;
+        //     }
+
+        //     currSize++;
+        //     HashNode* n = new HashNode(key, value);
+            
+        //     n->next = table[index];
+        //     table[index] = n;
+
+        //     cout << "Key [" << key << "] inserted at index ["  << index << "]." << endl;
+
+        // }
+
         void insert(string key, int value)
         {
             if (getLoadFactor() > 0.7)
@@ -126,21 +161,198 @@ class HashTable
 
             // now we check if the key already exists in our program 
             HashNode* trav = table[index];
+            // HashNode* prev = NULL;  //this is useless since we perform insertion at the front/head of the list 
+
+            while (trav != NULL)
+            {
+                if (trav->key == key)
+                {
+                    trav->value = value;
+                    cout << "key [" << key << "] updated." << endl;
+                    return;
+                }
+
+                // prev = trav;  //this is useless since we perform insertion at the front/head of the list 
+                trav = trav->next;
+            }
+
+            // when it passes through the above loop without returning we know for a fact that the particular key did not exist in our program 
+            HashNode* newNode = new HashNode(key, value);
+            currSize++;
+
+            newNode->next = table[index];
+            table[index] = newNode; 
+
+            cout << "Key [" << key << "] inserted at index " << index << "." << endl;
+ 
+        }
+
+        void remove(string key)
+        {
+            int index = hashFunction(key);
+
+            HashNode* trav = table[index];
             HashNode* prev = NULL;
 
             while (trav != NULL)
             {
                 if (trav->key == key)
                 {
-                    
+                    // if the node to be deleted is at the front/head of the queue (not necessary that it is the only element in the chain at that)
+                    // particular index position in the haahtable
+                    if (prev == NULL)
+                    {
+                        table[index] = trav->next;
+                    }
+                    // if the node in positioned somewhere in between or at the end of the chain then the procedure for deletion would be a little different
+                    else
+                    {
+                        prev->next = trav->next;
+                    } 
+
+                    delete trav;
+                    currSize--;
+                    cout << "Key [" << key << "] removed." << endl;
+                    return;
                 }
+                prev = trav;
+                trav = trav->next;
             }
+
+            cout << "Key [" << key << "] not found, could not remove." << endl;
+
+        }
+
+        // void remove(string key)
+        // {
+
+        //     int index = hashFunction(key);
+
+        //     HashNode* trav = table[index];
+        //     HashNode* prev = NULL;
+
+        //     while (trav != NULL)
+        //     {
+        //         if (trav->key == key)
+        //         {
+        //             if (prev == NULL)
+        //             {
+        //                 table[index] = trav->next;
+        //             }
+        //             else 
+        //             {
+        //                 prev->next = trav->next;
+        //             }
+
+        //             delete trav;
+        //             currSize--;
+
+        //             cout << "Key [" << key << "] removed." << endl;
+        //             return;
+        //         }
+
+        //         prev = trav;
+        //         trav = trav->next;
+                
+        //     }
+
+        //     cout << "Key [" << key << "] not found, could not remove." << endl;
+        // }
+
+        bool search(string key, int &resultValue)
+        {
+            int index = hashFunction(key);
+
+            HashNode* trav = table[index];
+
+            while(trav != NULL)
+            {
+                if (trav->key == key)
+                {
+                    resultValue = trav->value;
+                    return true; 
+                }
+
+                trav = trav->next;
+            }
+            return false;
+        }
+
+        void display()
+        {
+
+            cout << "\n------- Current Hash Table State -------\n";
+
+            for (int m = 0; m < numBuckets; m++)
+            {
+                cout << "Bucket "<< m << ": " << endl;  
+                HashNode* trav = table[m];
+
+                if (trav == NULL)
+                {
+                    cout << "Empty////" << endl;
+                }
+                else 
+                {
+                    while (trav != NULL)
+                    {
+                        cout << "{" << trav->key << "," << trav->value << "}" << "->";
+                        trav = trav->next;
+                    }
+
+                    cout << "NULL" << endl;
+                }
+
+                cout << endl;
+            }
+
+            cout << "-------------------------------------------------\n";
 
         }
 };
 
 int main(void)
 {
+
+    HashTable studentsGrades(5);
+
+    cout << "=== Hash Table from scratch (NO STL) ===" << endl; 
+
+    // Insertion forcing collisions
+    studentsGrades.insert("Abu Bakar", 80);
+    studentsGrades.insert("Ali", 30);
+    studentsGrades.insert("usama", 2);
+    studentsGrades.insert("Fatima", 49);
+    studentsGrades.insert("Waqar", 23);
+
+    // using display()
+    studentsGrades.display();
+
+    // moving onto searching////
+
+    int gradeStorer = 0;
+
+    string searchkey = "Abu Bakar";
+
+    if (studentsGrades.search(searchkey, gradeStorer))
+    {
+        cout << "Found " << searchkey << ":" << gradeStorer << endl;
+    }
+    else
+    {
+        cout << searchkey << " not found." << endl;
+    }
+
+    // updating
+
+    cout << "\nUpdating Ali's grade: "  << endl;
+    studentsGrades.insert("Ali", 100);
+
+    // removing
+
+    cout << "\nRemoving Waqar////" << endl;
+    studentsGrades.remove("Waqar");
     
+    studentsGrades.display();
     return 0;
 }
