421f3a400998d376dc9f1809ecfbffd883e92a19
Muhammad Abu Bakar <muhammadabubakermemon@gmail.com>
2025-12-14 10:22:00 +0500

    In Sha Allah full in final, well because we don't have another option now do we after that Sessional 2 disasterclass///

---
+++ LabTasks/Lab08/Q2.cpp
@@ -0,0 +1,5 @@
+// 2. A platform tracks user based on their engagement score, which changes daily as they
+// complete quizzes or watch lectures. Design a BST that stores user profiles and their
+// engagement scores. It can efficiently find the most/least active learners. It should be able
+// to supports deletion when users deactivate their accounts. Then use traversal to produce
+// a “leaderboard” of engagement.
